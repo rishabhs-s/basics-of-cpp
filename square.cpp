364db92c0f85dc34bea2bbab4c9d59dced81e86d
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-09-05 00:09:12 +0530

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter number to square "<<endl;
+	cin>>x;
+	int res=x*x;
+	cout<<"the square is :"<<res;
+	return 0;
+}
