8554f795834cf0e82c9efc405cfeee74788ab070
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-09-05 20:17:34 +0530

    inline function

---
+++ inline.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+inline int sum(int,int); //global function can be called from anywhere
+int main()
+{
+	int a=9,b=5;//a and b are actual arguments
+	int s=sum(a,b);
+	cout<<"sum is : "<<s;
+	
+}
+
+int sum(int x,int y)//x and y are formal arguments
+{
+	return(x+y);
+}
+
+
