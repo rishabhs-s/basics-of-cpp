2fcdbc3a786f5ddf991c2de9ae729cccf757e487
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-09-06 17:11:30 +0530

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+class construct{
+	private:
+		int a;
+	public:
+	construct(){
+		cout<<"Constructor called""\n";
+		}
+		
+};
+int main()
+{
+	construct c1;
+	construct c2,c3,c4;//for every object constructor is called 
+}
